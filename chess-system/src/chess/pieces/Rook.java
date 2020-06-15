a9ec6a771663870fcc176a5bce4fa8868ce9f521
Yan Andreatta <andreattadelima@gmail.com>
2020-06-15 16:08:46 -0300

    Printing possible moves

--- chess-system/src/chess/pieces/Rook.java
+++ chess-system/src/chess/pieces/Rook.java
@@ -43,17 +43,17 @@ public class Rook extends ChessPiece {
 		}
 
 		// right
-		p.setValues(position.getRow(), position.getColumn() - 1);
+		p.setValues(position.getRow(), position.getColumn() + 1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
-			p.setColumn(p.getColumn() - 1);
+			p.setColumn(p.getColumn() + 1);
 		}
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 
 		// below
-		p.setValues(position.getRow() - 1, position.getColumn());
+		p.setValues(position.getRow() + 1, position.getColumn());
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 			p.setRow(p.getRow() + 1);
